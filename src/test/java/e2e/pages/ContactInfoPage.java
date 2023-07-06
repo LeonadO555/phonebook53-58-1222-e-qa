3d83adfaccb6b83105674b8a51b08509dc2670ac
H_Voronkova <gallinavoronkova@gmail.com>
2023-07-06 22:18:37 +0200

    Created contact via API and edited contact via UI

---
+++ src/test/java/e2e/pages/ContactInfoPage.java
@@ -0,0 +1,95 @@
+package e2e.pages;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactInfoPage extends ContactBasePage{
+
+    public ContactInfoPage(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy (xpath = "//*[@name='input-ec-firstName']")
+    WebElement firstNameInput;
+
+    @FindBy (xpath = "//*[@name='input-ec-lastName']")
+    WebElement lastNameInput;
+
+    @FindBy (xpath = "//*[@name='input-ec-description']")
+    WebElement descriptionTextArea;
+
+    @FindBy (xpath = "//*[@class ='col-sm-3']//*[@type='submit']")
+    WebElement saveButton;
+
+    @FindBy(xpath = "//*[@class='toast-body']")
+    WebElement toast;
+
+    @FindBy(xpath = "//*[@id='contact-first-name']")
+    WebElement contactFirstName;
+
+    @FindBy(xpath = "//*[@id='contact-last-name']")
+    WebElement contactLastName;
+
+    @FindBy(xpath = "//*[@id='contact-description']")
+    WebElement contactDescription;
+
+    @FindBy(xpath = "//*[@id='btn-edit-contact']")
+    WebElement editButton;
+
+    public void waitForLoading() {
+        getWait().forVisibility(editButton);
+        getWait().forClickable(editButton);
+        getWait().forVisibility(contactFirstName);
+        getWait().forVisibility(contactLastName);
+        getWait().forVisibility(contactDescription);
+    }
+
+    public void waitForEditForm(){
+        getWait().forVisibility(firstNameInput);
+        getWait().forVisibility(lastNameInput);
+        getWait().forVisibility(descriptionTextArea);
+    }
+
+
+    public void setEditForm(String firstName, String lastName, String description){
+        firstNameInput.click();
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+        lastNameInput.click();
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+        descriptionTextArea.click();
+        descriptionTextArea.clear();
+        descriptionTextArea.sendKeys(description);
+    }
+
+    public boolean saveChanges(){
+        saveButton.click();
+        try{
+            saveButton.isDisplayed();
+            return true;
+        }catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void handleSuccessfulToast(){
+        Assert.assertTrue(toast.isDisplayed(), "Toast is not visible"); //после нажатия на save, мы эту кнопку больше не должны видеть
+        String toastText = toast.getText();
+        Assert.assertEquals(toastText, "Contact changed");
+    }
+
+    public List<String> getEditForm(){
+        List<String> listEditedData = new ArrayList<>();
+        listEditedData.add(contactFirstName.getText());
+        listEditedData.add(contactLastName.getText());
+        listEditedData.add(contactDescription.getText());
+        return listEditedData;
+    }
+}
