aedc06d0fba68362fdae782f2367683e4605ee5d
LeonadO555 <lmikhailov@supplyshift.net>
2023-07-11 21:25:09 +0400

    Described pages for test createContactViaApiEditContactViaUiTest

---
+++ src/test/java/enums/ContactTabs.java
@@ -0,0 +1,15 @@
+package enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ContactTabs {
+    INFO("//*[@ng-reflect-_id='1']"),
+    PHONES("//*[@ng-reflect-_id='2']"),
+    EMAILS("//*[@ng-reflect-_id='3']"),
+    ADDRESSES("//*[@ng-reflect-_id='4']");
+
+    public final String value;
+}
