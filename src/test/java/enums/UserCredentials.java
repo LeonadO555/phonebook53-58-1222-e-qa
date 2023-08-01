866b23837e603f4e9d05dfcb0d89e3ef64eea293
Irasp <iraspjob@gmail.com>
2023-08-01 23:01:58 +0300

    Added LoginTest

---
+++ src/test/java/enums/UserCredentials.java
@@ -0,0 +1,18 @@
+package enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserCredentials {
+    VALID_EMAIL("test@gmail.com"),
+    VALID_PASSWORD("test@gmail.com"),
+    INVALID_EMAIL("222gmail.com"),
+    INVALID_PASSWORD("test222@gmail.com");
+
+    public final String value;
+
+    UserCredentials(String value) {
+        this.value = value;
+    }
+}
+
