291f7967bfbbd0e1f17073eca935a60a98c51ef1
H_Voronkova <gallinavoronkova@gmail.com>
2023-08-31 21:03:49 +0200

    finished PhonePage

--- src/test/java/e2e/pages/PhonePage.java
+++ src/test/java/e2e/pages/PhonePage.java
@@ -2,9 +2,11 @@ package e2e.pages;
 
 import enums.CountryCodes;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class PhonePage extends ContactBasePage {
 
@@ -19,20 +21,20 @@ public class PhonePage extends ContactBasePage {
     @FindBy(xpath = "//*[@class='btn btn-info text-white']")
     WebElement addPhoneNumberButton;
 
+    @FindBy(xpath = "//*[@role='dialog']")
+    WebElement dialog;
+
     @FindBy(xpath = "//*[@class='custom-select']")
     WebElement countryCodeDropdown;
 
     @FindBy(xpath = "//*[@id='selected-cc']")
-    WebElement phoneNumberField;
+    WebElement phoneNumberInput;
 
     @FindBy(xpath = "//*[@class='btn btn-primary']")
     WebElement savePhoneButton;
 
     @FindBy(xpath = "//*[@class='toast-body']")
     WebElement successfulToast;
-
-    @FindBy(xpath = "//*[@class= 'd-block fade modal show']")
-    WebElement addPhoneNumberForm;
 
     @FindBy(xpath = "//*[@class = 'dropdown-item btn-phone-edit']")
     WebElement editButton;
@@ -43,22 +45,45 @@ public class PhonePage extends ContactBasePage {
     public void waitForLoading() {
         getWait().forVisibility(addPhoneNumberButton);
         getWait().forClickable(addPhoneNumberButton);
-        getWait().forVisibility(countryCodeDropdown);
-        getWait().forVisibility(phoneNumberField);
-        getWait().forVisibility(savePhoneButton);
     }
 
+    public void waitForDialog() {
+        getWait().forVisibility(dialog);
+    }
 
-    public void selectCountryCodes(CountryCodes countryCodes) {
-        selectDropdownText(countryCodeDropdown, countryCodes.code);
+    public void setForm(CountryCodes countryCode, String phoneNumber) {
+        selectDropdownText(countryCodeDropdown, countryCode.code);
+        phoneNumberInput.click();
+        phoneNumberInput.clear();
+        phoneNumberInput.sendKeys(phoneNumber);
+    }
+
+    public boolean saveChanges() {
+        savePhoneButton.click();
+        try {
+            savePhoneButton.isDisplayed();
+            return true;
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void handleSuccessfulToast() {
+        Assert.assertTrue(successfulToast.isDisplayed(), "Toast is not visible");
+        String toastText = successfulToast.getText();
+        Assert.assertEquals(toastText, "Phone  changed");
     }
 
     public WebElement makeCellRow(String phone) {
         return driver.findElement(By.xpath("//*[contains(@ng-reflect-result, '" + phone + "')]"));
     }
 
+    public boolean isVisiblePhone(String phone) {
+        return makeCellRow(phone).isDisplayed();
+    }
+
     public void openDropdown(String phone) { //gear button
         driver.findElement(By.xpath("//*[contains(@ng-reflect-result, '" + phone + ")]/ancestor::tr//*[@class='nav-item ml-auto dropdown']")).click();
     }
-
 }
