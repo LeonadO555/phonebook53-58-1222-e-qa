b50e948869213eff23f769c31e8c4f324d1026e9
H_Voronkova <gallinavoronkova@gmail.com>
2023-09-17 19:01:36 +0200

    finished sorting email

--- src/test/java/e2e/SortingEmailForContactHW.java
+++ src/test/java/e2e/SortingEmailForContactHW.java
@@ -2,6 +2,7 @@ package e2e;
 
 import api.contact.Contact;
 import com.github.javafaker.Faker;
+import e2e.pages.ContactInfoPage;
 import e2e.pages.ContactPage;
 import e2e.pages.EmailPage;
 import e2e.pages.LoginPage;
@@ -13,6 +14,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingEmailForContactHW extends TestBase {
@@ -21,67 +24,58 @@ public class SortingEmailForContactHW extends TestBase {
     LoginPage loginPage;
     ContactPage contactPage;
 
+    ContactInfoPage contactInfoPage;
+
     EmailPage emailPage;
 
     Faker faker = new Faker();
 
     @Test
-    public void sortingEmailForContactHW() {
+    public void sortingEmailForContactHW() throws InterruptedException {
+
+        contact = new Contact();
+        JsonPath createdContact = contact.createContact(201).jsonPath();
+        int contactId = createdContact.getInt("id");
 
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
         loginPage.login(UserCredentials.VALID_EMAIL, UserCredentials.VALID_PASSWORD);
         loginPage.confirmSuccessfulLogin();
 
-        contact = new Contact();
-        JsonPath createdContact = contact.createContact(201).jsonPath();
-        int id = createdContact.getInt("id");
-
         contactPage = new ContactPage(app.driver);
         contactPage.waitForLoading();
-        contactPage.openContactById(String.valueOf(id));
-        
+        contactPage.openContactById(String.valueOf(contactId));
+
+        contactInfoPage = new ContactInfoPage(app.driver);
+        contactInfoPage.waitForLoading();
+
         emailPage = new EmailPage(app.driver);
         emailPage.clickOnEmailsTab(ContactTabs.EMAILS);
+        List<String> emailsList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             String contactEmail = faker.internet().emailAddress();
+            emailsList.add(contactEmail);
             emailPage.openAddDialog();
-            emailPage.waitForLoading();
             emailPage.setEmail(contactEmail);
             emailPage.clickSaveButton();
-//            emailPage.handleSuccessfulToast();
+            Thread.sleep(1000);
             emailPage.waitForEmailPage();
             Assert.assertTrue(emailPage.makeCellEmail(contactEmail));
-            emailPage.waitForEmailPage();
         }
-
-        List<String> expectedListEmailsUp = new ArrayList<>();
-        expectedListEmailsUp.add("wiliam@gmail.com");
-        expectedListEmailsUp.add("peter@meta.net");
-        expectedListEmailsUp.add("mary@hotline.com");
-        expectedListEmailsUp.add("arthur@barmer.net");
-
-
-        List<String> expectedListEmailsDown = new ArrayList<>();
-        expectedListEmailsDown.add("arthur@barmer.net");
-        expectedListEmailsDown.add("mary@hotline.com");
-        expectedListEmailsDown.add("peter@meta.net");
-        expectedListEmailsDown.add("wiliam@gmail.com");
-
 
         emailPage.clickOnSortEmailLink();
         emailPage.checkSortDirection(SortDirection.UP);
         List<String> actualListEmailUp = emailPage.getTextFromEmailCell();
-        Assert.assertEquals(actualListEmailUp, expectedListEmailsUp, "Actual list of emails is not equals expected");
-
+        Collections.sort(emailsList, Comparator.reverseOrder());
+        Assert.assertEquals(actualListEmailUp, emailsList, "Actual list of emails is not equals expected");
 
         emailPage.clickOnSortEmailLink();
-        emailPage.checkSortDirection(SortDirection.DOWN);
         List<String> actualListEmailDown = emailPage.getTextFromEmailCell();
-        Assert.assertEquals(actualListEmailDown, expectedListEmailsDown, "Actual list of emails is not equals expected");
+        Collections.sort(emailsList, Comparator.naturalOrder());
+        Assert.assertEquals(actualListEmailDown, emailsList, "Actual list of emails is not equals expected");
 
-        contact.deleteContact(200, id);
-        JsonPath actualDeletedContact = contact.getContact(500, id).jsonPath();
+        contact.deleteContact(200, contactId);
+        JsonPath actualDeletedContact = contact.getContact(500, contactId).jsonPath();
         Assert.assertEquals(actualDeletedContact.getString("message"), "Error! This contact doesn't exist in our DB");
     }
 }
